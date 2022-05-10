4eb4c665520f4d45e253554b080676cad2e9d740
Matthew <tykecomputing@gmail.com>
2022-05-10 17:10:09 +0100

    HeaderFooter v0.21

--- js/header.js
+++ js/header.js
@@ -12,8 +12,10 @@ headerTemplate.innerHTML = `
       height: 30px;
       padding: 1px;
       display: flex;
+      margin: auto;
+      max-width: 650px;
       align-items: center;
-      justify-content: center;
+      justify-content: space-between;
       background-color:  white;
     }
 
@@ -28,7 +30,6 @@ headerTemplate.innerHTML = `
     
     nav a {
       font-weight: 500;
-      margin: 0 12px;
       color: #000;
       text-decoration: none;
       font-size: 12px;
@@ -36,16 +37,17 @@ headerTemplate.innerHTML = `
     }
     
     nav a:hover {
-      padding-bottom: 5px;
-      box-shadow: inset 0 -2px 0 0 #fff;
-      background-colour: #ddd;
+      /*box-shadow: inset 0 -2px 0 0 #fff;
+      background-color: #ddd;
+      z-index:; -1;*/
+      text-decoration: underline;
     }
 
     h1 {
-      font-size: 2em;
-      font-weight: 500;
+      font-size: 34px;
+      font-weight: bold;
       font-family: kepler-w03-light-scd-cp,serif;
-      /*font: normal normal normal 2em 'eb garamond',serif;*/
+      font: normal normal normal 2em 'eb garamond',serif;
       text-align: center;
       letter-spacing: 0.2em;
       margin-top: 0.67em;
@@ -57,8 +59,10 @@ headerTemplate.innerHTML = `
     .headerbanner {
       display: flex;
       margin: auto;
-      margin-bottom: 10px;
+      margin-top: 10px;
+      margin-bottom: 15px;
       width: 80%;
+      max-width: 960px;
       justify-content: space-around;
       align-items: center;
       position: relative;
@@ -72,7 +76,10 @@ headerTemplate.innerHTML = `
       justify-content: center;
       align-items: center;
       width: 50%;
+      min-width: 500px;
       max-height: 125px;margin-top: -10px;
+      margin-left: 20px;
+      margin-right: 20px;
     }
 
     .titlesubtext {
@@ -80,50 +87,55 @@ headerTemplate.innerHTML = `
       margin-bottom: 5px;
       letter-spacing: 0.2em;
       font-family: kepler-w03-light-scd-cp,serif;
-      font-size: 30px;
       font-weight: bold;
       line-height: normal;
       min-height: 27px;
-      font: normal normal normal 17px/1.4em 'eb garamond',serif;
+      font: normal normal normal 14px/1.4em 'eb garamond',serif;
     }
 
     .leftlogos {
       display: flex;
       width: 30%;
-      justify-content: space-evenly;
+      justify-content: space-between;
       align-items: center;
     }
 
     #arlogo {
-      width: 80%;
+      width: 90%;
+      display: flex;
     }
 
     #siglogo {
+      display: flex;
     }
 
     .rightlogos {
       display: flex;
       width: 30%;
-      justify-content: space-evenly;
+      justify-content: space-between;
       align-items: center;
-      margin: auto;
     }
 
     #mdalogo {
-      
+      margin: auto;
+      display: flex;
     }
 
     #acelogo {
-      width: 45%;
+      display: flex;
+      margin: auto;
+      width: 65%;
     }
 
     #spotlightlogo {
-      width: 45%;
+      display: flex;
+      width: 55%;
     }
 
     .menubar {
       background: url("img/banner.png") repeat-x 0 100%;
       min-height: 8px;
+      z-index: 1;
     }
 
     /*Dropdown Content - Hidden by default*/
@@ -133,23 +145,22 @@ headerTemplate.innerHTML = `
       background-color: white;
       min-width: 160px;
       box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-      z-index: 1;
+      z-index: 2;
       align: center;
     }
 
     /*Links inside the dropdown*/
     .dropdown-content a {
       color: black;
-      padding: 12px 16px;
-      text-decoration: none;
+      padding: 1px 16px;
       display: block;
     }
 
     /*Same effect on dropdown hover as standard a links*/
     .dropdown:hover {
-      padding-bottom: 5px;
-      box-shadow: inset 0 -2px 0 0 #fff;
-      background-colour: #ddd;
+      /*box-shadow: inset 0 -2px 0 0 #fff;
+      background-color: #ddd;*/
+      text-decoration: underline;
     }
 
     /*Show  dropdown menu  on hover*/
@@ -185,33 +196,39 @@ headerTemplate.innerHTML = `
     </div>
     <nav class="navbar">
       <!--TODO: Create Drop Downs within the menu-->
-      <a href="index.html">HOME</a>
-      <a href="about.html">ABOUT</a>
+      <a href="index.html"><div class="linkbox"><p class="linkp">HOME</p></div></a>
+      <a href="about.html"><div class="linkbox"><p class="linkp">ABOUT</p></div></a>
       <div class="dropdown">
-        <a href="performance/performance.html">PERFORMANCE</a>
+        <a href="performance/performance.html"><div class="linkbox"><p class="linkp">PERFORMANCE</p></div></a>
           <div class="dropdown-content">
-            <a href="performance/theatre/theatre.html">THEATRE</a>
-            <a href="performance/screen/screen.html">SCREEN</a>
-            <a href="performance/puppetry/puppetry.html">PUPPETRY</a>
-            <a href="performance/voice/voice.html">VOICE</a>
+            <a href="performance/theatre/theatre.html"><div class="sublinkbox"><p class="linkp">THEATRE</p></div></a>
+            <a href="performance/screen/screen.html"><div class="sublinkbox"><p class="linkp">SCREEN</p></div></a>
+            <a href="performance/puppetry/puppetry.html"><div class="sublinkbox"><p class="linkp">PUPPETRY</p></div></a>
+            <a href="performance/voice/voice.html"><div class="sublinkbox"><p class="linkp">VOICE</p></div></a>
           </div>
       </div>
       <div class="dropdown">
-        <a href="creative/creative.html">CREATIVE</a>
+        <a href="creative/creative.html"><div class="linkbox"><p class="linkp">CREATIVE</p></div></a>
           <div class="dropdown-content">
-            <a href="creative/directing/directing.html">DIRECTING</a>
-            <a href="creative/movement-directing/movement-directing.html">MOVEMENT DIRECTING</a>
-            <a href="creative/producing/producing.html">PRODUCING</a>
-            <a href="creative/facilitation/facilitation.html">FACILITATION</a>
+            <a href="creative/directing/directing.html"><div class="sublinkbox"><p class="linkp">DIRECTING</p></div></a>
+            <a href="creative/movement-directing/movement-directing.html"><div class="sublinkbox"><p class="linkp">MOVEMENT DIRECTING</p></div></a>
+            <a href="creative/producing/producing.html"><div class="sublinkbox"><p class="linkp">PRODUCING</p></div></a>
+            <a href="creative/facilitation/facilitation.html"><div class="sublinkbox"><p class="linkp">FACILITATION</p></div></a>
           </div>
       </div>
       <div class="dropdown">
-        <a href="teaching/teaching.html">TEACHING</a>
+        <a href="teaching/teaching.html"><div class="linkbox"><p class="linkp">TEACHING</p></div></a>
           <div class="dropdown-content">
             <!--TODO: Teaching Pages design-->
           </div>
       </div>
-      <a href="contact.html">CONTACT</a>
+      <div class="dropdown">
+      <a href="storytelling/storytelling.html"><div class="linkbox"><p class="linkp">STORYTELLING</p></div></a>
+        <div class="dropdown-content">
+          <!--TODO: Storytelling Pages design-->
+        </div>
+    </div>
+      <a href="contact.html"><div class="linkbox"><p class="linkp">CONTACT</p></div></a>
     </nav>
     <div class="menubar">
     </div>
